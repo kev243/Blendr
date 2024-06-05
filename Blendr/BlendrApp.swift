281edd0f32408529b96e5a4c20f7d41e3e54c4ca
Kevin Nimi <k.nimi73@gmail.com>
2024-06-05 15:07:30 -0400

    Initial Commit

---
+++ Blendr/BlendrApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlendrApp.swift
+//  Blendr
+//
+//  Created by Kevin Nimi on 2024-06-05.
+//
+
+import SwiftUI
+
+@main
+struct BlendrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
